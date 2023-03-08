5f968b3dfa0b955a0162025dae07505d69825b27
Brandon Ros <brandonros1@gmail.com>
2023-03-08 14:49:49 -0500

    checkin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # teamcity-dsl-lib-example
+
+```shell
+mvn clean install
+# output is target/TeamCityDslLibExample-1.0-SNAPSHOT.jar
+# use with com.brandonros.test.CustomProject
+```
---
+++ src/main/kotlin/com/brandonros/test/Sources.kt
@@ -0,0 +1,11 @@
+package com.brandonros.test
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+object Sources : GitVcsRoot({
+    name = "Sources"
+    url = DslContext.getParameter("repoUrl")
+})
